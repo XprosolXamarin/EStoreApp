fe41a43536de16bbb206682cd28cfb30afbf548d
Luqman Akram <luqmanakram00300@outlook.com>
2020-12-02 04:49:51 -0800

    PushNotification

---
+++ EStoreApp/EStoreApp.Android/MyFirebaseIIDService.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Firebase.Iid;
+
+namespace EStoreApp.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            SendRegistrationToServer(refreshedToken);
+        }
+        void SendRegistrationToServer(string token)
+        {
+            // send this token to server
+        }
+    }
+}
